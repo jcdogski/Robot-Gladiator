ee3a086423d9c6254068637b2d7ee62dd87a24be
jcdogski <jacobcarcerano@gmail.com>
2021-10-15 20:55:14 -0700

    added java script lines

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,16 @@
-var playerName = window.prompt("what is your robots name?")
-function fight(){
-    alert("The fight has begun!");
-}
-// fight()
-console.log(playerName)
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
-var name = "your name";
-console.log(name);
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
