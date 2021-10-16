054f7cfda4321b92f4a3ef4b8bc3edfbdc025c30
jcdogski <jacobcarcerano@gmail.com>
2021-10-15 19:26:14 -0700

    added prompts to javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robots name?")
+function fight(){
+    alert("The fight has begun!");
+}
+// fight()
+console.log(playerName)
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+var name = "your name";
+console.log(name);
